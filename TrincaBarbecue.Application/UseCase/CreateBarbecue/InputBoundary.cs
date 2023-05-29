f5ef1a26ec5b31d0b0e276016dd74ebbb5a9d841
Yuri Melo <yurifullstack@gmail.com>
2023-05-29 18:40:36 -0300

    Update code

--- TrincaBarbecue.Application/UseCase/CreateBarbecue/InputBoundary.cs
+++ TrincaBarbecue.Application/UseCase/CreateBarbecue/InputBoundary.cs
@@ -1,13 +1,15 @@
-﻿namespace TrincaBarbecue.Application.UseCase.CreateBarbecue
+﻿using TrincaBarbecue.Core;
+
+namespace TrincaBarbecue.Application.UseCase.CreateBarbecue
 {
-    public class InputBoundary
+    public class InputBoundary : IInputBoundary
     {
         public DateTime BeginDate { get; private set; }
         public DateTime EndDate { get; private set; }
         public string Description { get; private set; } = string.Empty;
-        public IEnumerable<string> AdditionalObservations { get; private set; } = new List<string>();
+        public List<string> AdditionalObservations { get; private set; } = new List<string>();
 
-        private InputBoundary(string description, IEnumerable<string> additionalObservations, DateTime beginDate, DateTime endDate)
+        private InputBoundary(string description, List<string> additionalObservations, DateTime beginDate, DateTime endDate)
         {
             Description = description;
             AdditionalObservations = additionalObservations;
@@ -15,7 +17,7 @@
             EndDate = endDate;
         }
 
-        public static InputBoundary FactoryMethod(string description, IEnumerable<string> additionalObservations, DateTime beginDate, DateTime endDate)
+        public static InputBoundary FactoryMethod(string description, List<string> additionalObservations, DateTime beginDate, DateTime endDate)
         {
             var barbebueEntity = new InputBoundary(description, additionalObservations, beginDate, endDate);
             return barbebueEntity;
