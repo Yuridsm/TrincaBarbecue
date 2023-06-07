38d12ecf7084bda91ff5e10fc6d0119a884fc563
Yuri Melo <yurifullstack@gmail.com>
2023-06-07 14:32:31 -0300

    Dispatch all domain event by using MediatR

---
+++ SummitPro.SharedKernel/DomainEventDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SummitPro.SharedKernel.Interfaces;
+
+namespace SummitPro.SharedKernel
+{
+    public class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly IMediator _mediator;
+
+        public DomainEventDispatcher(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task DispatchAndClearEvents(IEnumerable<EntityBase> entitiesWithEvents)
+        {
+            foreach (var entity in entitiesWithEvents)
+            {
+                var events = entity.DomainEvents.ToArray();
+                entity.ClearDomainEvent();
+
+                foreach (var domainEvent in events)
+                {
+                    await _mediator.Publish(domainEvent);
+                }
+            }
+        }
+    }
+}
