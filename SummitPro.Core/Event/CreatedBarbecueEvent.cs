633fb59dc47520e8a2bbc8d2114d357a756b0644
Yuri Melo <yurifullstack@gmail.com>
2023-06-07 15:14:17 -0300

    Add events

---
+++ SummitPro.Core/Event/CreatedBarbecueEvent.cs
@@ -0,0 +1,16 @@
+﻿using SummitPro.SharedKernel;
+
+namespace SummitPro.Core.Event
+{
+    public class CreatedBarbecueEvent : DomainEventBase
+    {
+        public string Name { get; init; }
+        public Guid BarbecueIdentifier { get; init; }
+
+        public CreatedBarbecueEvent(string name, Guid barbecueIdentifier)
+        {
+            Name = name;
+            BarbecueIdentifier = barbecueIdentifier;
+        }
+    }
+}
