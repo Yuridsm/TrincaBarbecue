442cf3ec23dd0a56e305b7c5f303339f3e9ee99c
Yuri Melo <yurifullstack@gmail.com>
2023-06-14 09:09:24 -0300

    Bind participant command handler has been created

---
+++ SummitPro.Application/Command/Handler/BindParticipantHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SummitPro.Application.Repository;
+using SummitPro.SharedKernel.Messaging;
+
+namespace SummitPro.Application.Command.Handler
+{
+    public class BindParticipantHandler : ICommandHandler<BindParticipantCommand, Unit>
+    {
+        private readonly IBarbecueRepository _barbecueRepository;
+        private readonly IParticipantRepository _participantRepository;
+
+        public BindParticipantHandler(IBarbecueRepository barbecueRepository, IParticipantRepository participantRepository)
+        {
+            _barbecueRepository = barbecueRepository;
+            _participantRepository = participantRepository;
+        }
+
+        public Task<Unit> Handle(BindParticipantCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
