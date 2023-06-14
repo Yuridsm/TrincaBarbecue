49792d2141f5accef4aa6d21b0578ae3f8fc0677
Yuri Melo <yurifullstack@gmail.com>
2023-06-14 09:07:32 -0300

    Component command model has removed

--- SummitPro.Application/CommandModel/CreateComponentCommandModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SummitPro.Application.CommandModel
-{
-    public class CreateComponentCommandModel
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-    }
-}
