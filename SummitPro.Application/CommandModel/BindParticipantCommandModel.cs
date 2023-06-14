71725b4203350f79867deaee1e9e8093540c7a58
Yuri Melo <yurifullstack@gmail.com>
2023-06-14 09:08:13 -0300

    BindParticipant command model has been created

---
+++ SummitPro.Application/CommandModel/BindParticipantCommandModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SummitPro.Application.CommandModel
+{
+    public class BindParticipantCommandModel
+    {
+        public Guid BarbecueIdentifier { get; set; }
+        public Guid ParticipantIdentifier { get; set; }
+    }
+}
