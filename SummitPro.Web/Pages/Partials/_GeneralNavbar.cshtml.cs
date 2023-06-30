341c922ef226e67824f37e4dc9a38f90db8a6825
Yuri Melo <yurifullstack@gmail.com>
2023-06-30 00:06:54 -0300

    Add partial

---
+++ SummitPro.Web/Pages/Partials/_GeneralNavbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SummitPro.Web.Pages.Partials
+{
+    public class _GeneralNavbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
