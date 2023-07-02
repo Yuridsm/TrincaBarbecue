8acfce071c5c40c2f95d144f22306990334c7af2
Yuri Melo <yurifullstack@gmail.com>
2023-07-02 17:11:08 -0300

    Change namespace

--- SummitPro.Web/Pages/Barbecue/Create.cshtml.cs
+++ SummitPro.Web/Pages/Barbecue/Create.cshtml.cs
@@ -4,49 +4,48 @@ using SummitPro.Application.Interface;
 using SummitPro.Application.UseCase.CreateBarbecue;
 using SummitPro.Web.ViewModel;
 
-namespace SummitPro.Web.Pages.Barbecue
+namespace SummitPro.Web.Pages.Barbecue;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
-    {
-        private readonly ICreateBarbecueUseCase _createBarbecueUseCase;
+	private readonly ICreateBarbecueUseCase _createBarbecueUseCase;
 
-        [BindProperty]
-        public BarbecueViewModel Barbecue { get; set; }
+	[BindProperty]
+	public BarbecueViewModel Barbecue { get; set; }
 
-        public CreateModel(ICreateBarbecueUseCase createBarbecueUseCase)
-        {
-            Barbecue = new BarbecueViewModel();
-            _createBarbecueUseCase = createBarbecueUseCase;
-        }
+	public CreateModel(ICreateBarbecueUseCase createBarbecueUseCase)
+	{
+		Barbecue = new BarbecueViewModel();
+		_createBarbecueUseCase = createBarbecueUseCase;
+	}
 
-        public void OnGet()
-        {
-        }
+	public void OnGet()
+	{
+	}
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid) return Page();
+	public async Task<IActionResult> OnPostAsync()
+	{
+		if (!ModelState.IsValid) return Page();
 
-            Console.WriteLine(Barbecue);
+		Console.WriteLine(Barbecue);
 
-            var splittedAdditionalRemarks = Barbecue.AdditionalRemarks[0].Split("\r\n");
+		var splittedAdditionalRemarks = Barbecue.AdditionalRemarks[0].Split("\r\n");
 
-            foreach(var remark in splittedAdditionalRemarks)
-                Barbecue.AdditionalRemarks.Add(remark);
+		foreach (var remark in splittedAdditionalRemarks)
+			Barbecue.AdditionalRemarks.Add(remark);
 
-            Barbecue.AdditionalRemarks.RemoveAt(0);
+		Barbecue.AdditionalRemarks.RemoveAt(0);
 
-            var inputBoundary = new CreateBarbecueInputBoundary
-            {
-                Description = Barbecue.Description,
-                BeginDate = Barbecue.BeginDate,
-                EndDate = Barbecue.EndDateTime,
-                AdditionalObservations = Barbecue.AdditionalRemarks
-            };
+		var inputBoundary = new CreateBarbecueInputBoundary
+		{
+			Description = Barbecue.Description,
+			BeginDate = Barbecue.BeginDate,
+			EndDate = Barbecue.EndDateTime,
+			AdditionalObservations = Barbecue.AdditionalRemarks
+		};
 
-            var output = await _createBarbecueUseCase.Execute(inputBoundary);
+		var output = await _createBarbecueUseCase.Execute(inputBoundary);
 
-            return RedirectToPage("Index");
-        }
-    }
+		return RedirectToPage("Index");
+	}
 }
