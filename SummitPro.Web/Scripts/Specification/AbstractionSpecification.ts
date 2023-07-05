74310167d41aae3bd55c575f47f9dcb003034d73
Yuri Melo <yurifullstack@gmail.com>
2023-07-05 20:32:33 -0300

    Add Specification Pattern in order to validate fields in HTML

---
+++ SummitPro.Web/Scripts/Specification/AbstractionSpecification.ts
@@ -0,0 +1,21 @@
+﻿import { ISpecification } from "./ISpecification";
+
+export abstract class AbstractSpecification<T> implements ISpecification<T> {
+    abstract isSatisfiedBy(item: T): boolean;
+
+    isSatisfiedBy(element: T): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    and(other: ISpecification<T>): ISpecification<T> {
+        throw new Error("Method not implemented.");
+    }
+
+    or(other: ISpecification<T>): ISpecification<T> {
+        throw new Error("Method not implemented.");
+    }
+
+    not(other: ISpecification<T>): ISpecification<T> {
+        throw new Error("Method not implemented.");
+    }
+}
