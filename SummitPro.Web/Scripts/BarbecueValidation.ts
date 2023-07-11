3e31af3c0025ad18b11528369b06b4fa23ece8fd
Yuri Melo <yurifullstack@gmail.com>
2023-07-11 13:42:56 -0300

    Remove form validation function

--- SummitPro.Web/Scripts/BarbecueValidation.ts
+++ SummitPro.Web/Scripts/BarbecueValidation.ts
@@ -1,11 +1,8 @@
-﻿import FieldIsNotNullSpecification from "./Specification/FieldIsNotNullSpecification";
-import BeginDateTimeInputValidation from "./Validation/BeginDateTimeInputValidation";
+﻿import BeginDateTimeInputValidation from "./Validation/BeginDateTimeInputValidation";
 import DescriptionInputValidation from "./Validation/DescriptionInputValidation";
 import EndDateTimeInputValidation from "./Validation/EndDateTimeInputValidation";
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    const form = document.getElementById('create-barbecue-identifier') as HTMLFormElement;
     const description = document.getElementById('description') as HTMLInputElement;
     const beginDateTime = document.getElementById('begin-datetime') as HTMLInputElement;
     const endDateTime = document.getElementById('end-datetime') as HTMLInputElement;
@@ -17,20 +14,4 @@ document.addEventListener('DOMContentLoaded', () => {
     DescriptionInputValidation.execute(description, descriptionInputErrorMessage);
     BeginDateTimeInputValidation.execute(beginDateTime, endDateTime, beginDateTimeErrorMessage, endDateTimeErrorMessage);
     EndDateTimeInputValidation.execute(beginDateTime, endDateTime, beginDateTimeErrorMessage, endDateTimeErrorMessage);
-
-    form.addEventListener('submit', (event: Event) => {
-        event.preventDefault();
-
-        const description = document.getElementById('description') as HTMLInputElement;
-        const descriptionErrorMessage = document.getElementById('description-error-message') as HTMLSpanElement;
-
-        const fieldIsNotNullSpecification = new FieldIsNotNullSpecification();
-
-        if (!fieldIsNotNullSpecification.isSatisfiedBy(description.value)) {
-            descriptionErrorMessage.textContent = 'Description is required';
-        }
-        else {
-            descriptionErrorMessage.textContent = '';
-        }
-    });
 });
