fddd752aa3b975f0d0a0a5f22f8cb6c83c8294a4
Yuri Melo <yurifullstack@gmail.com>
2023-11-23 00:48:00 -0300

    Remove SummitPro.WebUI angular project

--- SummitPro.WebUI/src/app/models/participant.ts
+++
@@ -1,8 +0,0 @@
-export class Participant {
-    Identifier = "";
-    Name = "";
-    ContributionValue: number = 0;
-    BringDrink: boolean = false;
-    Items: string[]  = [];
-    Username  = "";
-}
